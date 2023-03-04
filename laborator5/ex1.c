c50f2e7ea39c14751582abffd3fad1998d8e154b
anastasiasandu <95322927+anastasiasandu@users.noreply.github.com>
2023-03-04 03:27:08 +0200

    Update ex1.c

--- laborator5/ex1.c
+++ laborator5/ex1.c
@@ -11,17 +11,16 @@
 int main(int argc, char *argv[])
 {
     int i;
-    printf(" %d\n", getpid());
-    //parintele creeaza ob de mem part
+    printf(" %d\n", getpid()); 
     char *shm_name="lab5";  
     int shm_fd;
-    shm_fd=shm_open(shm_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);  //creez ob de mem partajata lab5, r si w doar utilizatorului
+    shm_fd=shm_open(shm_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);   
     if(shm_fd<0)
     {
         return errno;
     }
     size_t page_size=getpagesize(); 
-    size_t shm_size=page_size*argc; //impart memoria in mod egal pentru fiecare copil 
+    size_t shm_size=page_size*argc;  
     if(ftruncate(shm_fd, shm_size)==-1)  
 	{ 
 		return errno;
@@ -29,7 +28,7 @@ int main(int argc, char *argv[])
 	char *shm_ptr; 
 	for(i=1; i<argc; i++)
     {
-        shm_ptr=mmap(0, page_size, PROT_WRITE, MAP_SHARED, shm_fd, (i-1)*page_size); //incarc memoria partajata in spatiul procesului
+        shm_ptr=mmap(0, page_size, PROT_WRITE, MAP_SHARED, shm_fd, (i-1)*page_size);  
         if(shm_ptr==MAP_FAILED)
 		{ 
 			return errno;
@@ -42,7 +41,7 @@ int main(int argc, char *argv[])
         else if(pid==0) //copil
         {
             int x=atoi(argv[i]);
-            shm_ptr+=sprintf(shm_ptr, "%d: %d ", x, x); //formatez outputul
+            shm_ptr+=sprintf(shm_ptr, "%d: %d ", x, x);  
             while(x!=1)
             {
                 if(x%2==0) x/=2;
@@ -55,7 +54,7 @@ int main(int argc, char *argv[])
     }
     for(i=1; i<argc; i++) 
     {
-        wait(NULL);  //parintele asteapta ca fiecare copil sa termine executia
+        wait(NULL);   
     }
     for(i=1; i<argc; i++)
     {
