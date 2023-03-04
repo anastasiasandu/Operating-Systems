edb9656d7f7fa167151326f0f8f6beb6456b9c1c
anastasiasandu <95322927+anastasiasandu@users.noreply.github.com>
2023-03-04 03:28:01 +0200

    Update ex2.c

--- laborator5/ex2.c
+++ laborator5/ex2.c
@@ -11,17 +11,16 @@
 int main(int argc, char *argv[])
 {
     int i;
-    printf(" %d\n", getpid());
-    //parintele creeaza ob de mem part
+    printf(" %d\n", getpid()); 
     char *shm_name="lab5";  
     int shm_fd;
-    shm_fd=shm_open(shm_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);//creez ob de mem partajata lab5, r si w doar utilizatorului
+    shm_fd=shm_open(shm_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR); 
     if(shm_fd<0)
     {
         return errno;
     }
     size_t page_size=getpagesize(); 
-    size_t shm_size=page_size*argc;   //impart memoria in mod egal pentru fiecare copil 
+    size_t shm_size=page_size*argc;   
     if(ftruncate(shm_fd, shm_size)==-1)  
 	{
         shm_unlink(shm_name); 
@@ -30,7 +29,7 @@ int main(int argc, char *argv[])
 	char *shm_ptr; 
 	for(i=1; i<argc; i++)
     {
-        shm_ptr=mmap(0, page_size, PROT_WRITE, MAP_SHARED, shm_fd, (i-1)*page_size); //incarc memoria partajata in spatiul procesului
+        shm_ptr=mmap(0, page_size, PROT_WRITE, MAP_SHARED, shm_fd, (i-1)*page_size);  
         if(shm_ptr==MAP_FAILED)
 		{
             shm_unlink(shm_name);    //sterg obiectul creat
@@ -41,7 +40,7 @@ int main(int argc, char *argv[])
         else if(pid==0) //copil
         {
             int x=atoi(argv[i]);
-            shm_ptr+=sprintf(shm_ptr, "%d: %d ", x, x); //formatez outputul, incarcam in zona de memorie nr din ipoteza collatz
+            shm_ptr+=sprintf(shm_ptr, "%d: %d ", x, x); 
             while(x!=1)
             {
                 if(x%2==0) x/=2;
@@ -51,13 +50,13 @@ int main(int argc, char *argv[])
             printf("  %d   %d\n",  getppid(), getpid());
             return 1;
         }
-        munmap(shm_ptr,page_size);  //eliberez zona de memorie
+        munmap(shm_ptr,page_size);   
     }
     for(i=1; i<argc; i++) 
     {
-        wait(NULL);   //parintele asteapta ca fiecare copil sa isi termine executia
+        wait(NULL);   
     }
-    for(i=1; i<argc; i++) //afisez procesele-copil (fiecare cu zona de memorie)
+    for(i=1; i<argc; i++)  
     {
         shm_ptr=mmap(0, page_size, PROT_READ, MAP_SHARED, shm_fd, (i-1)*page_size);
 		printf("%s\n", shm_ptr);
